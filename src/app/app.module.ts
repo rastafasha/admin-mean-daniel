f7b9534ad466448dae7859db708e328ea22a5ea0
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-05-04 17:50:31 -0400

    arreglado activacion interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PipesModule } from './pipes/pipes.module';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
 import { AuthModule } from './auth/auth.module';
 import { RouterModule } from '@angular/router';
 
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './http-interceptors/auth-interceptor';
-import { PaypalInterceptor } from './http-interceptors/paypal-interceptor';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { AuthInterceptor } from './http-interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -37,11 +34,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     })
   ],
   providers: [
-    // {
-    //   provide: HTTP_INTERCEPTORS,
-    //   useClass: PaypalInterceptor,
-    //   multi: true
-    // }
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      // useClass: PaypalInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
